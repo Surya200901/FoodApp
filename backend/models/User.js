c1ab992eab376c695b9552dda520366b7f6c1ec6
Surya1934 <settipallisurya1919@gmail.com>
2025-11-05 17:56:54 +0530

    Removed backend and node_modules from Git tracking

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true, sparse: true },
-  phone: { type: String, unique: true, sparse: true },
-  password: { type: String, required: true }
-});
-
-module.exports = mongoose.model('User', UserSchema);
